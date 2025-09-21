e1435287dc9fe0c92ea47209d38c9733ff54288b
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-21 15:54:04 +0000

    Implement users card components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -10,4 +10,17 @@ const User: React.FC<UserProps> = ({ name, email }) => (
     </div>
 );
 
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+
 export default User;
