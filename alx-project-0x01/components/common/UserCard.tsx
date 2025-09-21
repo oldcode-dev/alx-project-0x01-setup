d9905317df6b50b8567e3707f5467d3c9394fe1f
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-21 16:20:32 +0000

    feat: modified UserCard component to receive an array

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,18 +1,26 @@
 import { UserProps } from "@/interfaces";
 
-const PostCard: React.FC<UserProps> = ({ id, username, email }) => {
-  return (
-    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="mb-4">
-        <h2 className="text-2xl font-semibold text-gray-800">{username}</h2>
-      </div>
-      <p className="text-gray-600">{email}</p>
-      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
-        <span>User ID: {username}</span>
-        <span>Post ID: {id}</span>
-      </div>
-    </div>
-  );
+const UserCard: React.FC<UserProps[]> = (props) => {
+    return (
+        <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            {
+                props.map(({ id, name, username, email }) => {
+                    return (
+                        <>
+                            <div className="mb-4">
+                                <h2 className="text-2xl font-semibold text-gray-800">{name}</h2>
+                            </div>
+                            <p className="text-gray-600">{email}</p>
+                            <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+                                <span>User ID: {username}</span>
+                                <span>Post ID: {id}</span>
+                            </div>
+                        </>
+                    )
+                })
+            }
+        </div>
+    );
 };
 
-export default PostCard;
+export default UserCard;
